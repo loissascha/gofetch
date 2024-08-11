9c62431def61567b4ec3e6aa1c0425cab89853ff
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-11 21:37:02 +0200

    create default files and config folder if it doesn't exist

--- config.go
+++ config.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"io/fs"
+	"os"
+)
+
+var configDirPath string
+var configDir []fs.DirEntry
+
 func initConfig() {
+	homeDir, _ := os.UserHomeDir()
+	configDirPath = homeDir + "/.config/go-fetch-tool"
+	var err error
+	configDir, err = os.ReadDir(configDirPath)
+	if err != nil {
+		createDefaultConfig()
+		configDir, err = os.ReadDir(configDirPath)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func createDefaultConfig() {
+	os.Mkdir(configDirPath, 0755)
+	//os.WriteFile(configDirPath+"config.cfg", []byte(""), 0755)
+
+	artDirPath := configDirPath + "/art"
+	infoDirPath := configDirPath + "/info"
+	os.Mkdir(artDirPath, 0755)
+	os.Mkdir(infoDirPath, 0755)
+
+	os.WriteFile(artDirPath+"/pikachu.txt", []byte(` \033[30m░░░░░░░░\033[33m▀████▀▄▄\033[30m░░░░░░░░░░░░░░\033[33m▄█
+\033[30m░░░░░░░░░░\033[33m█▀\033[30m░░░░\033[33m▀▀▄▄▄▄▄\033[30m░░░░\033[33m▄▄▀▀█
+\033[30m░░\033[33m▄\033[30m░░░░░░░░\033[33m█\033[30m░░░░░░░░░░\033[33m▀▀▀▀▄\033[30m░░\033[33m▄▀
+\033[30m░\033[33m▄▀\033[30m░\033[33m▀▄\033[30m░░░░░░\033[33m▀▄\033[30m░░░░░░░░░░░░░░\033[33m▀▄▀
+\033[33m▄▀\033[30m░░░░\033[33m█\033[30m░░░░░\033[33m█▀\033[30m░░░\033[33m▄█▀▄\033[30m░░░░░░\033[33m▄█
+\033[33m▀▄\033[30m░░░░░\033[33m▀▄\033[30m░░\033[33m█\033[30m░░░░░\033[33m▀██▀\033[30m░░░░░\033[33m██▄█
+\033[30m░\033[33m▀▄\033[30m░░░░\033[33m▄▀\033[30m░\033[33m█\033[30m░░░\033[33m▄██▄\033[30m░░░\033[33m▄\033[30m░░\033[33m▄\033[30m░░\033[33m▀▀\033[30m░\033[33m█
+\033[30m░░\033[33m█\033[30m░░\033[33m▄▀\033[30m░░\033[33m█\033[30m░░░░\033[33m▀██▀\033[30m░░░░\033[33m▀▀\033[30m░\033[33m▀▀\033[30m░░\033[33m▄▀
+\033[30m░\033[33m█\033[30m░░░\033[33m█\033[30m░░\033[33m█\033[30m░░░░░░\033[33m▄▄\033[30m░░░░░░░░░░░\033[33m▄▀ `), 0755)
+
+	os.WriteFile(infoDirPath+"/minimal.txt", []byte(`\033[1m\033[33m[*user*]@[*hostname*]
+\033[1m[*osName*]
+[*kernelVersion*]
+[*desktopSession*]
+[*cpuModel*]
+[*uptime*]`), 0755)
 
 }
