d0e126487cb1de5e00bb28abe0d23b8b0efa13d5
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-12 10:42:05 +0200

    get random art

--- art.go
+++ art.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 )
 
 func getArt() []string {
-	file, err := os.Open(configDirPath + "/art/pikachu.txt")
+	fname := getRandomArtFile()
+	file, err := os.Open(configDirPath + "/art/" + fname)
 	if err != nil {
 		fmt.Println("art not found")
 		return []string{}
@@ -23,4 +25,18 @@ func getArt() []string {
 		result = append(result, l)
 	}
 	return result
+}
+
+func getRandomArtFile() string {
+	fs, err := os.ReadDir(configDirPath + "/art")
+	if err != nil {
+		fmt.Println("art directory not found")
+		return ""
+	}
+	if len(fs) == 0 {
+		fmt.Println("art directory is empty")
+		return ""
+	}
+	i := rand.Intn(len(fs))
+	return fs[i].Name()
 }
