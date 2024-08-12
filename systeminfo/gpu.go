cf47eb9c731156d20ffab25cbe4529a4df0567f3
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-12 14:36:48 +0200

    extract gpu model if possible

--- systeminfo/gpu.go
+++ systeminfo/gpu.go
@@ -16,5 +16,34 @@ func (s *SystemInfo) loadGpuModel() {
 	st := string(r)
 	st = strings.TrimSpace(st)
 	st = strings.TrimSuffix(st, "\n")
+
+	st = s.extractGpuModel(st)
+
 	s.gpuModel = st
+}
+
+func (s *SystemInfo) extractGpuModel(st string) string {
+	runTest := true
+	ss := st
+	res := ""
+	for runTest {
+		_, after, foundStart := strings.Cut(ss, "[")
+		if foundStart {
+			before, end, foundEnd := strings.Cut(after, "]")
+			if foundEnd {
+				res += before + " "
+				ss = end
+			} else {
+				runTest = false
+				break
+			}
+		} else {
+			runTest = false
+			break
+		}
+	}
+	if res == "" {
+		res = st
+	}
+	return res
 }
